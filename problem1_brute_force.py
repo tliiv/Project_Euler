79062d4ddd3391dd7606c1ceaaeb7106c14374ef
tliiv <toomas.liiv@outlook.com>
2016-10-28 19:24:29 +0200

    Solution for problem 1 through brute force looping

---
+++ problem1_brute_force.py
@@ -0,0 +1,37 @@
+# This is intended to be a solution to problem 1 on Project EUler
+# https://projecteuler.net/problem=1
+# Toomas Liiv 2016-10-28
+
+# This is a brute force solution.
+# However, for fun, we implement it generally, for an upper limit n and 
+# a list of factors instead of just 3 and 5.
+
+def multiple_sum(n, factors):
+	'''Computes the sum of all numbers below n which are divisible by
+	   at least one element in the list factors. 
+
+	   Args: 
+	   		n (int)        : upper limit
+	   		factors (list) : list of factors
+
+	   Returns:
+	   		ans (int)      : sum of all numbers below n divisible by at 
+	   						 at least one element in factors
+
+	   Example:
+
+	   		>> multiple_sum(1000, [3, 5])
+	   		233168
+	'''
+	ans = 0
+	for i in range(n):
+		for el in factors:
+			if i%el == 0:
+				ans += i
+				break
+	return ans
+
+def main():
+	print(multiple_sum(1000,[3, 5]))
+
+main()
